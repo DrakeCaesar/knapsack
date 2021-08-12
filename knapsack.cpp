541e3fab365210b7da1b196e409d3f90c9830480
Dominik Cisár <dominik.cisar@gmail.com>
2021-08-12 15:54:47 +0200

    refactoring

--- knapsack.cpp
+++ knapsack.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 // CPP code for Dynamic Programming based
@@ -12,9 +15,105 @@ int max(int a, int b) { return (a > b) ? a : b; }
 int sumw;
 int X1050;
 int Baellie;
+
+stringstream sstream;
+ofstream MyFile("..//output.txt");
+
+struct Engine {
+    string name;
+    int weight;
+    int thrust;
+};
+
+vector<struct Engine> thrusters = {{"Chipmunk Plasma Thruster", 20, 34560},
+                                   {"Greyhound Plasma Thruster", 34, 66240},
+                                   {"Impala Plasma Thruster", 58, 127440},
+                                   {"Orca Plasma Thruster", 98, 244440},
+                                   {"Tyrant Plasma Thruster", 167, 469800},
+                                   {"X1700 Ion Thruster", 16, 21600},
+                                   {"X2700 Ion Thruster", 27, 41400},
+                                   {"X3700 Ion Thruster", 46, 79560},
+                                   {"X4700 Ion Thruster", 79, 153000},
+                                   {"X5700 Ion Thruster", 134, 293400},
+                                   {"A120 Atomic Thruster", 22, 55400},
+                                   {"A250 Atomic Thruster", 34, 98280},
+                                   {"A370 Atomic Thruster", 53, 171360},
+                                   {"A520 Atomic Thruster", 82, 294840},
+                                   {"A860 Atomic Thruster", 127, 502920},
+                                   {"Basrem Atomic Thruster", 18, 47520},
+                                   {"Benga Atomic Thruster", 28, 84960},
+                                   {"Biroo Atomic Thruster", 44, 149400},
+                                   {"Bondir Atomic Thruster", 63, 237960},
+                                   {"Bufaer Atomic Thruster", 104, 432360},
+                                   {"Type 1 Radiant Thruster", 12, 23760},
+                                   {"Type 2 Radiant Thruster", 27, 63360},
+                                   {"Type 3 Radiant Thruster", 42, 113400},
+                                   {"Type 4 Radiant Thruster", 65, 198720},
+                                   {"Thruster (Asteroid Class)", 14, 40320},
+                                   {"Thruster (Comet Class)", 24, 78480},
+                                   {"Thruster (Lunar Class)", 40, 148320},
+                                   {"Thruster (Planetary Class)", 69, 288000},
+                                   {"Thruster (Stellar Class)", 118, 552240},
+                                   {"Small Thrust Module", 9, 23760},
+                                   {"Large Thrust Module", 32, 94320},
+                                   {"Pug Akfar Thruster", 43, 100800},
+                                   {"Pug Cormet Thruster", 60, 158400},
+                                   {"Pug Lohmar Thruster", 84, 237600},
+                                   {"Medium Gravitron Thruster", 70, 288000},
+                                   {"Crucible Class Thruster", 20, 64800},
+                                   {"Forge Class Thruster", 39, 133200},
+                                   {"Smelter Class Thruster", 76, 276480},
+                                   {"X1050 Ion Engines", 20, 14400},
+                                   {"Baellie Atomic Engines", 24, 36360}
+
+};
+
+vector<struct Engine> steerings = {
+    {"Chipmunk Plasma Steering", 15, 15360},
+    {"Greyhound Plasma Steering", 26, 29520},
+    {"Impala Plasma Steering", 43, 56640},
+    {"Orca Plasma Steering", 74, 108720},
+    {"Tyrant Plasma Steering", 125, 208740},
+    {"X1200 Ion Steering", 12, 9600},
+    {"X2200 Ion Steering", 20, 18420},
+    {"X3200 Ion Steering", 35, 35400},
+    {"X4200 Ion Steering", 59, 67900},
+    {"X5200 Ion Steering", 100, 130440},
+    {"A125 Atomic Steering", 16, 23520},
+    {"A255 Atomic Steering", 25, 41220},
+    {"A375 Atomic Steering", 38, 71520},
+    {"A525 Atomic Steering", 60, 123000},
+    {"A865 Atomic Steering", 92, 210540},
+    {"Basrem Atomic Steering", 12, 18540},
+    {"Benga Atomic Steering", 20, 34620},
+    {"Biroo Atomic Steering", 32, 63240},
+    {"Bondir Atomic Steering", 49, 105480},
+    {"Bufaer Atomic Steering", 76, 182580},
+    {"Type 1 Radiant Steering", 9, 10368},
+    {"Type 2 Radiant Steering", 20, 27240},
+    {"Type 3 Radiant Steering", 30, 47160},
+    {"Type 4 Radiant Steering", 47, 83754},
+    {"Steering (Asteroid Class)", 10, 16800},
+    {"Steering (Comet Class)", 18, 34128},
+    {"Steering (Lunar Class)", 30, 63360},
+    {"Steering (Planetary Class)", 52, 124176},
+    {"Steering (Stellar Class)", 89, 240300},
+    {"Small Steering Miodule", 7, 10728},
+    {"Large Steering Module", 25, 42714},
+    {"Pug Akfar Steering", 33, 45000},
+    {"Pug Cormet Steering", 46, 67800},
+    {"Pug Lohmar Steering", 64, 102000},
+    {"Medium Gravitron Steering", 50, 96000},
+    {"Crucible Class Thruster", 14, 26880},
+    {"Forge Class Thruster", 28, 57120},
+    {"Smelter Class Thruster", 55, 118800},
+    {"X1050 Ion Engines", 20, 6600},
+    {"Baellie Atomic Engines", 24, 15000},
+};
+
 // Prints the items which are put in a knapsack of capacity W
-void printknapSack(int W, int wt[], int val[], int n, const char *names[],
-                   const char *word) {
+void printknapSack(int W, vector<struct Engine> engine, int n, const char *word,
+                   string *signature, stringstream *local) {
     X1050 = 0;
     Baellie = 0;
     sumw = 0;
@@ -29,9 +128,10 @@ void printknapSack(int W, int wt[], int val[], int n, const char *names[],
         for (w = 0; w <= W; w++) {
             if (i == 0 || w == 0)
                 K[i][w] = 0;
-            else if (wt[i - 1] <= w)
-                K[i][w] =
-                    max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else if (engine[i - 1].weight <= w)
+                K[i][w] = max(engine[i - 1].thrust +
+                                  K[i - 1][w - engine[i - 1].weight],
+                              K[i - 1][w]);
             else
                 K[i][w] = K[i - 1][w];
         }
@@ -40,7 +140,7 @@ void printknapSack(int W, int wt[], int val[], int n, const char *names[],
     // stores the result of Knapsack
     int res = K[n][W];
     int res1 = res;
-    // cout << "Thrust:"<< res << "\n";
+    // sstream << "Thrust:"<< res << "\n";
 
     w = W;
     for (i = n; i > 0 && res > 0; i--) {
@@ -55,102 +155,62 @@ void printknapSack(int W, int wt[], int val[], int n, const char *names[],
         else {
 
             // This item is included.
-            cout << "\t" << wt[i - 1] << "\t" << names[i - 1] << "\n";
-            sumw = sumw + wt[i - 1];
-            if (strcmp(names[i - 1], "X1050 Ion Engines") == 0)
+            stringstream temp;
+            temp << "\t" << engine[i - 1].weight << "\t" << engine[i - 1].name
+                 << "\n";
+            *signature = *signature + temp.str();
+            *local << "\t" << engine[i - 1].weight << "\t" << engine[i - 1].name
+                   << "\n";
+            sumw = sumw + engine[i - 1].weight;
+            if (strcmp(engine[i - 1].name.c_str(), "X1050 Ion Engines") == 0)
                 X1050++;
-            if (strcmp(names[i - 1], "Baellie Atomic Engines") == 0)
+            if (strcmp(engine[i - 1].name.c_str(), "Baellie Atomic Engines") ==
+                0)
                 Baellie++;
 
             // Since this weight is included its
             // value is deducted
-            res = res - val[i - 1];
-            w = w - wt[i - 1];
+            res = res - engine[i - 1].thrust;
+            w = w - engine[i - 1].weight;
         }
     }
-    cout << "Outfit: " << sumw << "\n";
-    cout << word << "\t" << res1 << "\n";
+    *local << "Outfit: " << sumw << "\n";
+    *local << word << "\t" << res1 << "\n";
 }
 
 // Driver code
 int main(int argc, char *argv[]) {
-    freopen("output.txt", "w", stdout);
-    if (argc != 2)
-        return 1;
-    int thrust[] = {34560,  66240,  127440, 244440, 469800, 21600,  41400,
-                    79560,  153000, 293400, 55400,  98280,  171360, 294840,
-                    502920, 47520,  84960,  149400, 237960, 432360, 23760,
-                    63360,  113400, 198720, 40320,  78480,  148320, 288000,
-                    552240, 23760,  94320,  100800, 158400, 237600, 288000,
-                    64800,  133200, 276480, 14400,  36360};
-    int tWeight[] = {20,  34, 58, 98, 167, 16,  27, 46, 79, 134, 22, 34, 53, 82,
-                     127, 18, 28, 44, 63,  104, 12, 27, 42, 65,  14, 24, 40, 69,
-                     118, 9,  32, 43, 60,  84,  70, 20, 39, 76,  20, 24};
-    char const *tNames[]{
-        "Chipmunk Plasma Thruster",  "Greyhound Plasma Thruster",
-        "Impala Plasma Thruster",    "Orca Plasma Thruster",
-        "Tyrant Plasma Thruster",    "X1700 Ion Thruster",
-        "X2700 Ion Thruster",        "X3700 Ion Thruster",
-        "X4700 Ion Thruster",        "X5700 Ion Thruster",
-        "A120 Atomic Thruster",      "A250 Atomic Thruster",
-        "A370 Atomic Thruster",      "A520 Atomic Thruster",
-        "A860 Atomic Thruster",      "Basrem Atomic Thruster",
-        "Benga Atomic Thruster",     "Biroo Atomic Thruster",
-        "Bondir Atomic Thruster",    "Bufaer Atomic Thruster",
-        "Type 1 Radiant Thruster",   "Type 2 Radiant Thruster",
-        "Type 3 Radiant Thruster",   "Type 4 Radiant Thruster",
-        "Thruster (Asteroid Class)", "Thruster (Comet Class)",
-        "Thruster (Lunar Class)",    "Thruster (Planetary Class)",
-        "Thruster (Stellar Class)",  "Small Thrust Module",
-        "Large Thrust Module",       "Pug Akfar Thruster",
-        "Pug Cormet Thruster",       "Pug Lohmar Thruster",
-        "Medium Gravitron Thruster", "Crucible Class Thruster",
-        "Forge Class Thruster",      "Smelter Class Thruster",
-        "X1050 Ion Engines",         "Baellie Atomic Engines"};
-
-    int steering[] = {15360,  29520, 56640,  108720, 208740, 9600,   18420,
-                      35400,  67900, 130440, 23520,  41220,  71520,  123000,
-                      210540, 18540, 34620,  63240,  105480, 182580, 10368,
-                      27240,  47160, 83754,  16800,  34128,  63360,  124176,
-                      240300, 10728, 42714,  45000,  67800,  102000, 96000,
-                      26880,  57120, 118800, 6600,   15000};
-    int sWeight[] = {15, 26, 43, 74, 125, 12, 20, 35, 59, 100, 16, 25, 38, 60,
-                     92, 12, 20, 32, 49,  76, 9,  20, 30, 47,  10, 18, 30, 52,
-                     89, 7,  25, 33, 46,  64, 50, 14, 28, 55,  20, 24};
-    char const *sNames[]{
-        "Chipmunk Plasma Steering",  "Greyhound Plasma Steering",
-        "Impala Plasma Steering",    "Orca Plasma Steering",
-        "Tyrant Plasma Steering",    "X1200 Ion Steering",
-        "X2200 Ion Steering",        "X3200 Ion Steering",
-        "X4200 Ion Steering",        "X5200 Ion Steering",
-        "A125 Atomic Steering",      "A255 Atomic Steering",
-        "A375 Atomic Steering",      "A525 Atomic Steering",
-        "A865 Atomic Steering",      "Basrem Atomic Steering",
-        "Benga Atomic Steering",     "Biroo Atomic Steering",
-        "Bondir Atomic Steering",    "Bufaer Atomic Steering",
-        "Type 1 Radiant Steering",   "Type 2 Radiant Steering",
-        "Type 3 Radiant Steering",   "Type 4 Radiant Steering",
-        "Steering (Asteroid Class)", "Steering (Comet Class)",
-        "Steering (Lunar Class)",    "Steering (Planetary Class)",
-        "Steering (Stellar Class)",  "Small Steering Miodule",
-        "Large Steering Module",     "Pug Akfar Steering",
-        "Pug Cormet Steering",       "Pug Lohmar Steering",
-        "Medium Gravitron Steering", "Crucible Class Thruster",
-        "Forge Class Thruster",      "Smelter Class Thruster",
-        "X1050 Ion Engines",         "Baellie Atomic Engines"};
-    int W = atoi(argv[1]);
-    for (int i = 0; i <= W; i++) {
-        cout << "---------------------------------------------\n";
-        cout << "Total:\t" << W << "\t";
-        cout << "Thrust:\t" << i << "\t";
-        cout << "Steer:\t" << W - i << "\n\n";
-
-        int n = sizeof(thrust) / sizeof(thrust[0]);
-        printknapSack(i, tWeight, thrust, n, tNames, "Thrust:");
-        cout << "\n";
-        n = sizeof(steering) / sizeof(steering[0]);
-        printknapSack(W - i, sWeight, steering, n, sNames, "Steer:");
-        cout << "---------------------------------------------\n";
+    int W;
+    if (argc != 2) {
+        W = 40;
+    } else {
+        W = atoi(argv[1]);
     }
+
+    string oldSig;
+    string newSig;
+
+    for (int i = 0; i <= W; i++) {
+        oldSig = "" + newSig;
+        newSig = "";
+        stringstream local;
+
+        local << "---------------------------------------------\n";
+        local << "Total:\t" << W << "\t";
+        local << "Thrust:\t" << i << "\t";
+        local << "Steer:\t" << W - i << "\n\n";
+
+        int n = thrusters.size();
+        printknapSack(i, thrusters, n, "Thrust:  ", &newSig, &local);
+        cout << newSig;
+        local << "\n";
+        n = steerings.size();
+        printknapSack(W - i, steerings, n, "Steering:", &newSig, &local);
+        local << "---------------------------------------------\n";
+        if (oldSig.compare(newSig) != 0)
+            sstream << local.str();
+    }
+    // cout << sstream.str();
+    MyFile << sstream.str();
     return 0;
 }
